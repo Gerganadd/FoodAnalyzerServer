6b7f2f690fb2ef272232a03378a76cc333c28362
Gergana <gerganadoncheva2003@gmail.com>
2024-08-14 20:48:38 +0300

    Move all used regular expressions into one class

---
+++ src/bg/sofia/uni/fmi/mjt/regexs/Regex.java
@@ -0,0 +1,9 @@
+package bg.sofia.uni.fmi.mjt.regexs;
+
+public class Regex {
+    public static String MATCH_ONLY_DIGITS = "[0-9]+";
+    public static String MATCH_ONLY_WORDS_SEPARATED_BY_SPACE = "[a-zA-z ]+";
+    public static String START_WITH = "^";
+    public static String END_WITH = "$";
+    public static String MATCH_SPACE = " ";
+}
