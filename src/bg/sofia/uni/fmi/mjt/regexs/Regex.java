9808865664b0a9b76342e62a757ad84a235df91e
Gergana <gerganadoncheva2003@gmail.com>
2024-08-14 21:38:06 +0300

    update regexs

--- src/bg/sofia/uni/fmi/mjt/regexs/Regex.java
+++ src/bg/sofia/uni/fmi/mjt/regexs/Regex.java
@@ -2,8 +2,13 @@ package bg.sofia.uni.fmi.mjt.regexs;
 
 public class Regex {
     public static String MATCH_ONLY_DIGITS = "[0-9]+";
-    public static String MATCH_ONLY_WORDS_SEPARATED_BY_SPACE = "[a-zA-z ]+";
+    public static String MATCH_ONLY_WORDS = "[a-zA-z ]+";
+    public static String MATCH_IMAGE_ATTRIBUTE = " ?--img=[\\w:\\\\.]+";
+    public static String MATCH_BARCODE_ATTRIBUTE = " ?--code=[0-9]+";
+    public static String MATCH_IMAGE_AND_BARCODE_ATTRIBUTES =
+            String.format("((%s)|(%s)){1,2}", MATCH_IMAGE_ATTRIBUTE, MATCH_BARCODE_ATTRIBUTE);
     public static String START_WITH = "^";
     public static String END_WITH = "$";
     public static String MATCH_SPACE = " ";
+    public static String MATCH_EQUALS_SIGN = "=";
 }
