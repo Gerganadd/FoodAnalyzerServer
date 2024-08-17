aec175129e7d5d6da7069ea87ef47679b1611166
Gergana <gerganadoncheva2003@gmail.com>
2024-08-18 00:10:14 +0300

    add functionality to save logs information to a file

--- src/bg/sofia/uni/fmi/mjt/database/FileManager.java
+++ src/bg/sofia/uni/fmi/mjt/database/FileManager.java
@@ -1,11 +1,16 @@
 package bg.sofia.uni.fmi.mjt.database;
 
+import bg.sofia.uni.fmi.mjt.logs.Logger;
+import bg.sofia.uni.fmi.mjt.logs.Status;
+
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FileManager {
@@ -14,16 +19,54 @@ public class FileManager {
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
 
+    public static void saveTo(List<String> info, String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            for (String row : info) {
+                writer.write(row);
+            }
+
+            String message = "Successfully wrote to the file=" + filename;
+            Logger.getInstance().addLog(Status.SUCCESSFUL_WRITE_IN_FILE, message);
+        } catch (IOException e) {
+            String message = "IO exception occurred with writhing the file=" + filename;
+            Logger.getInstance().addException(Status.UNABLE_TO_WRITE, message, e);
+
+            System.out.println("Problem with saving the info in file " + filename);
+        }
+    }
+
     public static void saveTo(Map<String, String> info, String filename) {
         try (FileWriter writer = new FileWriter(filename)) {
             for (Map.Entry entry : info.entrySet()) {
                 writer.write(String.format(ROW_FORMAT, entry.getKey(), entry.getValue()));
             }
 
-            System.out.println("Successfully wrote to the file.");
+            String message = "Successfully wrote to the file=" + filename;
+            Logger.getInstance().addLog(Status.SUCCESSFUL_WRITE_IN_FILE, message);
         } catch (IOException e) {
-            e.printStackTrace(); // to-do save the exception in exception file
+            String message = "IO exception occurred with writhing the file=" + filename;
+            Logger.getInstance().addException(Status.UNABLE_TO_WRITE, message, e);
+
+            System.out.println("Problem with saving the info in file " + filename);
         }
+    }
+
+    public static List<String> loadLinesFrom(String filename) {
+        List<String> result = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            reader.lines().forEach(result::add);
+
+            String message = "Successfully read from the file=" + filename;
+            Logger.getInstance().addLog(Status.SUCCESSFUL_READ_FROM_FILE, message);
+        } catch (IOException e) {
+            String message = "IO exception occurred with reading from the file=" + filename;
+            Logger.getInstance().addException(Status.UNABLE_TO_READ, message, e);
+
+            System.out.println("Problem with loading the info from file " + filename);
+        }
+
+        return result;
     }
 
     public static Map<String, String> loadFrom(String filename) {
@@ -38,9 +81,13 @@ public class FileManager {
                 result.put(key, value);
             });
 
-            System.out.println("Successfully read the file.");
+            String message = "Successfully read from the file=" + filename;
+            Logger.getInstance().addLog(Status.SUCCESSFUL_READ_FROM_FILE, message);
         } catch (IOException e) {
-            e.printStackTrace(); // to-do save the exception in exception file
+            String message = "IO exception occurred with reading from the file=" + filename;
+            Logger.getInstance().addException(Status.UNABLE_TO_READ, message, e);
+
+            System.out.println("Problem with loading the info from file " + filename);
         }
 
         return result;
