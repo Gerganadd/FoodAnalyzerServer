dd66bd4a7daa833abdf08f4d4564cbda585704a8
Gergana <gerganadoncheva2003@gmail.com>
2024-08-18 16:08:46 +0300

    Fix the buffer overflow issue in network communication

--- src/bg/sofia/uni/fmi/mjt/foods/Foods.java
+++ src/bg/sofia/uni/fmi/mjt/foods/Foods.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public record Foods(List<Food> foods) implements Serializable {
-    private static final String SEPARATOR = "#";
+    private static final String SEPARATOR = "#"; // for read and write data from file
+    private static final String DELIMITER = ", "; // for send data in the network
+    private static final int MAX_FOODS_COUNT = 10;
 
     public static Foods deserialize(String info) {
         String[] args = info.split(SEPARATOR);
@@ -25,5 +27,16 @@ public record Foods(List<Food> foods) implements Serializable {
                 .toList();
 
         return String.join(SEPARATOR, args);
+    }
+
+    @Override
+    public String toString() {
+        List<String> args = foods
+                .stream()
+                .limit(MAX_FOODS_COUNT)
+                .map(Food::toString)
+                .toList();
+
+        return String.join(DELIMITER, args);
     }
 }
