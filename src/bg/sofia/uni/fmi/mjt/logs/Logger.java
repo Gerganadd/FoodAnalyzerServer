48538aa24927478159e0e10262cb29094639a636
Gergana <gerganadoncheva2003@gmail.com>
2024-08-27 21:00:17 +0300

    Save the time when new exception/log is received

--- src/bg/sofia/uni/fmi/mjt/logs/Logger.java
+++ src/bg/sofia/uni/fmi/mjt/logs/Logger.java
@@ -2,6 +2,7 @@ package bg.sofia.uni.fmi.mjt.logs;
 
 import bg.sofia.uni.fmi.mjt.database.FileManager;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +10,8 @@ import java.util.List;
 public class Logger {
     private static final String DEFAULT_FILE_NAME = "ServerLogs.txt";
     private static final String DEFAULT_EXCEPTIONS_FILE_NAME = "ServerExceptionsLogs.txt";
-    private static final String LOGS_FORMAT = "%s: %s\n";
-    private static final String EXCEPTIONS_LOGS_FORMAT = "%s: %s, stackTreatise: %s\n";
+    private static final String LOGS_FORMAT = "%s - %s: %s \n";
+    private static final String EXCEPTIONS_LOGS_FORMAT = "%s - %s: %s, stackTreatise: %s \n";
 
     private static Logger instance;
     private List<String> logs = new ArrayList<>();
@@ -25,12 +26,15 @@ public class Logger {
     }
 
     public void addException(Status status, String message, Exception exception) {
+        List<StackTraceElement> stackTrace = Arrays.stream(exception.getStackTrace()).toList();
+
         exceptionsLogs.add(String.format(EXCEPTIONS_LOGS_FORMAT,
-                status, message, Arrays.stream(exception.getStackTrace()).toList()));
+                LocalDateTime.now(), status, message, stackTrace));
     }
 
     public void addLog(Status status, String message) {
-        logs.add(String.format(LOGS_FORMAT, status.toString(), message));
+        logs.add(String.format(LOGS_FORMAT,
+                LocalDateTime.now(), status, message));
     }
 
     public void saveLogs(String filename) {
@@ -42,7 +46,7 @@ public class Logger {
     }
 
     public void saveLogs() {
-        FileManager.saveTo(logs, DEFAULT_FILE_NAME);
+        saveLogs(DEFAULT_FILE_NAME);
     }
 
     public void saveExceptions(String filename) {
@@ -54,6 +58,6 @@ public class Logger {
     }
 
     public void saveExceptions() {
-        FileManager.saveTo(exceptionsLogs, DEFAULT_EXCEPTIONS_FILE_NAME);
+        saveExceptions(DEFAULT_EXCEPTIONS_FILE_NAME);
     }
 }
