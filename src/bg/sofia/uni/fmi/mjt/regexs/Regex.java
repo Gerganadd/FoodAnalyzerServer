561100ffdbe18e05d6d903dfa1f29624a6d2c4b3
Gergana <gerganadoncheva2003@gmail.com>
2024-09-01 17:36:12 +0300

    Update elements

--- src/bg/sofia/uni/fmi/mjt/regexs/Regex.java
+++ src/bg/sofia/uni/fmi/mjt/regexs/Regex.java
@@ -1,14 +1,15 @@
 package bg.sofia.uni.fmi.mjt.regexs;
 
 public class Regex {
-    public static String MATCH_ONLY_DIGITS = "[0-9]+";
-    public static String MATCH_ONLY_WORDS = "[a-zA-z ]+";
-    public static String MATCH_IMAGE_ATTRIBUTE = " ?--img=[\\w:\\\\.]+";
-    public static String MATCH_BARCODE_ATTRIBUTE = " ?--code=[0-9]+";
-    public static String MATCH_IMAGE_AND_BARCODE_ATTRIBUTES =
+    public static final String MATCH_SPECIAL_SYMBOLS = "[,;]*";
+    public static final String MATCH_ONLY_DIGITS = "[0-9]+";
+    public static final String MATCH_ONLY_WORDS = "[a-zA-z ]+";
+    public static final String MATCH_IMAGE_ATTRIBUTE = " ?--img=[\\w:\\\\.]+";
+    public static final String MATCH_BARCODE_ATTRIBUTE = " ?--code=[0-9]+";
+    public static final String MATCH_IMAGE_AND_BARCODE_ATTRIBUTES =
             String.format("((%s)|(%s)){1,2}", MATCH_IMAGE_ATTRIBUTE, MATCH_BARCODE_ATTRIBUTE);
-    public static String START_WITH = "^";
-    public static String END_WITH = "$";
-    public static String MATCH_SPACE = " ";
-    public static String MATCH_EQUALS_SIGN = "=";
+    public static final String START_WITH = "^";
+    public static final String END_WITH = "$";
+    public static final String MATCH_SPACE = " ";
+    public static final String MATCH_EQUALS_SIGN = "=";
 }
